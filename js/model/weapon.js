d4fd9f6cb6c0c7df1ca35ac12217c7b76962ee49
lykong <konglingy_lady@sina.com>
2014-12-30 11:21:55 +0800

    Add property.

--- js/model/weapon.js
+++ js/model/weapon.js
@@ -1,6 +1,7 @@
-function Weapon (name, attack) {
+function Weapon (name, attack, feature) {
   this.name = name;
   this.attack = attack;
-};
+  this.feature = feature;
+}
 
 module.exports = Weapon;
