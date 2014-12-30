9df0ff1d1fb7c16a7d9af9922601608d8afba04c
lykong <konglingy_lady@sina.com>
2014-12-30 10:02:14 +0800

    Add property weapon and armor in Player.

--- js/model/player.js
+++ js/model/player.js
@@ -1,8 +1,10 @@
-function Player(name, hp, attack, profession) {
+function Player(name, hp, attack, profession, weapon, armor) {
   this.name = name;
   this.hp = hp;
   this.attack = attack;
   this.profession = profession;
+  this.weapon = weapon;
+  this.armor = armor;
 }
 
 module.exports = Player;
