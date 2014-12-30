5dad478773b07a324798c54577e312f8a9bfb078
lykong <konglingy_lady@sina.com>
2014-12-30 18:59:27 +0800

    Add weapon feature Class

---
+++ js/model/weapon-feature.js
@@ -0,0 +1,17 @@
+function WeaponFeature(name, count, odds) {
+  this.name = name;
+  this.count = count;
+  this.odds = odds;
+}
+
+WeaponFeature.all = function() {
+  return [
+    new WeaponFeature("毒", 2, 0.4),
+    new WeaponFeature("火", 2, 0.4),
+    new WeaponFeature("冰", 1, 0.4),
+    new WeaponFeature("晕", 2, 0.4),
+    new WeaponFeature("致命一击", 1, 0.4)
+  ];
+};
+
+module.exports = WeaponFeature;
