f97bb626fcd7b69bda8f1e28ba46cb3405176451
ranyang <ranyang525@gmail.com>
2014-12-31 15:06:50 +0800

    console text

--- js/model/soldier.js
+++ js/model/soldier.js
@@ -19,26 +19,27 @@ Soldier.prototype.criticalStrikes = function(player) {
   var attackHarm = (this.attack + this.weapon.attack)*3;
 
   var soldier = "战士" + this.name;
+  var resultText = "";
 
   if(one < odd) {
     player.hp = player.hp - attackHarm;
-    var resultText = soldier + '用' + this.weapon.name + "攻击了普通人" + player.name + ',' +
+    resultText += soldier + '用' + this.weapon.name + "攻击了普通人" + player.name + ',' +
         soldier + "发动了" + this.weapon.feature.name + "," +
         player.name + "受到了" + attackHarm + "点伤害," + player.name + "生命值还剩：" + player.hp;
+
   } else {
     player.hp = player.hp - this.attack;
-    var resultText = soldier + '用' + this.weapon.name + "攻击了普通人" + player.name + ',' +
+    resultText += soldier + '用' + this.weapon.name + "攻击了普通人" + player.name + ',' +
         player.name + "受到了" + this.attack + "点伤害," + player.name + "生命值还剩：" + player.hp;
-
   }
-
+  console.log(resultText);
   return resultText;
 };
 
 Soldier.prototype.fireStrikes = function(player) {
   var one = Math.random();
   var odd = this.weapon.feature.odds;
-  console.log(one);
+
   var resultText = "";
 
   var fireHarm = this.weapon.attack;
@@ -52,12 +53,14 @@ Soldier.prototype.fireStrikes = function(player) {
     player.name + "受到了" + this.attack + "点伤害，" + player.name + "着火了，生命值还剩：" + player.hp + "\n";
 
     player.hp = player.hp - fireHarm;
+
     resultText += player.name + "受到了" + fireHarm + "点火焰伤害，" + player.name + "生命值还剩：" + player.hp;
   } else {
     resultText += soldier + '用' + this.weapon.name + "攻击了普通人" + player.name + '，' +
     player.name + "受到了" + this.attack + "点伤害，" + player.name + "生命值还剩：" + player.hp;
 
   }
+  console.log(resultText);
 
   return resultText;
 };
@@ -65,7 +68,6 @@ Soldier.prototype.fireStrikes = function(player) {
 Soldier.prototype.venomStrikes = function(player) {
   var one = Math.random();
   var odd = this.weapon.feature.odds;
-  console.log(one);
   var resultText = "";
 
   var fireHarm = this.weapon.attack;
@@ -85,9 +87,43 @@ Soldier.prototype.venomStrikes = function(player) {
     player.name + "受到了" + this.attack + "点伤害，" + player.name + "生命值还剩：" + player.hp;
 
   }
+  console.log(resultText);
 
   return resultText;
 };
 
+Soldier.prototype.dizzyStrikes = function(player) {
+  var one = Math.random();
+  var odd = this.weapon.feature.odds;
+
+  var resultText = "";
+
+  var count = this.weapon.feature.count - 1;
+
+  player.hp = player.hp - this.attack;
+
+  var soldier = "战士" + this.name;
+
+  if (one < odd) {
+    resultText += soldier + '用' + this.weapon.name + "攻击了普通人" + player.name + '，' +
+    player.name + "受到了" + this.attack + "点伤害，" + player.name + "晕倒了，生命值还剩：" + player.hp + "\n";
+
+    resultText += player.name + "晕倒了，无法攻击，眩晕还剩：" + count + "轮" + "\n";
+    player.hp = player.hp - this.attack;
+
+    if(player.hp > 0) {
+      resultText += soldier + '用' + this.weapon.name + "攻击了普通人" + player.name + '，' +
+      player.name + "受到了" + this.attack + "点伤害，" + player.name + "晕倒了，生命值还剩：" + player.hp ;
+    }
+
+  } else {
+    resultText += soldier + '用' + this.weapon.name + "攻击了普通人" + player.name + '，' +
+    player.name + "受到了" + this.attack + "点伤害，" + player.name + "生命值还剩：" + player.hp;
+
+  }
+  console.log(resultText);
+
+  return resultText;
+};
 
 module.exports = Soldier;
