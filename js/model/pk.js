9ec28f56058a500d413e96040e53412836ad22fa
ranyang <ranyang525@gmail.com>
2014-12-31 15:07:30 +0800

    add pk.js

---
+++ js/model/pk.js
@@ -0,0 +1,53 @@
+var Soldier = require("./soldier.js");
+var Civilian = require("./civilian.js");
+
+function Pk() {
+
+}
+
+Pk.startPk = function (soldier,civilian) {
+
+    var resultText = "";
+
+    var features = soldier.weapon.feature;
+
+    while(true){
+        if(soldier.hp <= 0) {
+            resultText += soldier.name + "被打败了！！！";
+            break;
+        }
+        this.getAttackMethod(features, soldier, civilian);
+
+        if(civilian.hp <= 0) {
+            resultText += civilian.name + "被打败了！！！";
+            break;
+        }
+        civilian.civilianBeat(soldier, soldier.armor);
+    }
+    console.log(resultText);
+};
+
+Pk.getAttackMethod = function (feature, soldier, civilian) {
+
+    if(feature.name === '致命一击') {
+        return soldier.criticalStrikes(civilian);
+    }
+
+    if(feature.name === '火') {
+        return soldier.fireStrikes(civilian);
+    }
+
+    if(feature.name === '毒') {
+        return soldier.venomStrikes(civilian);
+    }
+
+    if(feature.name === '晕') {
+        return soldier.dizzyStrikes(civilian);
+    }
+
+    if(feature.name === '冰') {
+        return soldier.iceStrikes(civilian);
+    }
+};
+
+module.exports = Pk;
