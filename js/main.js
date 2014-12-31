cb76a9d11dbbca4f333d1b01739f39de584fecd2
ranyang <ranyang525@gmail.com>
2014-12-31 15:05:45 +0800

    modified property

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-var Result = require("./model/result.js");
-var Player = require("./model/player.js");
+var Pk = require("./model/pk.js");
 var Weapon = require("./model/weapon.js");
 var Armor = require("./model/armor.js");
+var Soldier = require("./model/soldier.js");
+var Civilian = require("./model/civilian.js");
+var WeaponFeature = require("./model/weapon-feature.js");
+
 
 game();
 function game() {
-  var playerOne = new Player("张三", 100, 20, "战士");
-  var playerTwo = new Player("李四", 100, 10, "普通人");
 
-  var weapon = new Weapon("无尽之刃", 5);
+  var feature = WeaponFeature.all();
+
+  var weapon = new Weapon("暴剑", 3, feature[3]);
   var armor = new Armor("荆棘之甲", 3);
 
-  Result.getResult(playerOne, playerTwo, weapon, armor);
+  var soldier = new Soldier("张三", 300, 20, weapon, armor);
+
+  var civilian = new Civilian("李四", 300, 10, "普通人");
+
+
+  Pk.startPk(soldier, civilian);
 }
