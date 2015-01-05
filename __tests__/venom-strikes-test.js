58ae40f524613a00400ea88037febcad21f646b4
lykong <konglingy_lady@sina.com>
2015-01-05 09:51:49 +0800

    Delete spece and format.

--- __tests__/venom-strikes-test.js
+++ __tests__/venom-strikes-test.js
@@ -4,7 +4,6 @@ jest.dontMock("../js/model/weapon.js");
 jest.dontMock("../js/model/player");
 jest.dontMock("../js/model/weapon-feature");
 
-
 describe("Soldier", function() {
   describe(".venomStrikes", function() {
     var Player = require("../js/model/player");
@@ -14,7 +13,6 @@ describe("Soldier", function() {
     var WeaponFeature = require("../js/model/weapon-feature");
 
     it("should return the correct statement", function() {
-
       var feature =new WeaponFeature("毒", 2, 1);
 
       var weapon = new Weapon("电光毒龙钻", 3, feature);
@@ -29,8 +27,8 @@ describe("Soldier", function() {
       "李四受到了3点毒液伤害，李四生命值还剩：77";
       expect(result).toBe(str);
     });
-    it("should return the correct statement", function() {
 
+    it("should return the correct statement", function() {
       var feature =new WeaponFeature("毒", 2, 0);
 
       var weapon = new Weapon("电光毒龙钻", 3, feature);
@@ -44,6 +42,5 @@ describe("Soldier", function() {
       var str = "战士张三用电光毒龙钻攻击了普通人李四，李四受到了20点伤害，李四生命值还剩：80";
       expect(result).toBe(str);
     });
-
   });
 });
