5906d8945bd47b652f8781f930a16c4b2ad80391
lykong <konglingy_lady@sina.com>
2015-01-05 09:50:24 +0800

    Delete spece and format.

--- __tests__/fire-strikes-test.js
+++ __tests__/fire-strikes-test.js
@@ -4,7 +4,6 @@ jest.dontMock("../js/model/weapon.js");
 jest.dontMock("../js/model/player");
 jest.dontMock("../js/model/weapon-feature");
 
-
 describe("Soldier", function() {
   describe(".fireStrikes", function() {
     var Player = require("../js/model/player");
@@ -14,7 +13,6 @@ describe("Soldier", function() {
     var WeaponFeature = require("../js/model/weapon-feature");
 
     it("should return the correct statement", function() {
-
       var feature = new WeaponFeature("火", 2, 1);
 
       var weapon = new Weapon("火箭", 3, feature);
@@ -29,6 +27,7 @@ describe("Soldier", function() {
          "李四受到了3点火焰伤害，李四生命值还剩：77";
       expect(result).toBe(str);
     });
+
     it("should return the correct statement", function() {
       var feature = new WeaponFeature("火", 2, 0);
 
@@ -43,5 +42,5 @@ describe("Soldier", function() {
       var str = "战士张三用火箭攻击了普通人李四，李四受到了20点伤害，李四生命值还剩：80";
       expect(result).toBe(str);
     });
-    });
+  });
 });
