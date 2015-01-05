8b05869d68987c5faa2d9aa5ef57903522daddc8
lykong <konglingy_lady@sina.com>
2015-01-05 11:31:22 +0800

    Change property.

--- js/main.js
+++ js/main.js
@@ -10,10 +10,11 @@ game();
 function game() {
   var feature = WeaponFeature.all();
 
-  var weapon = new Weapon("紫光冰魄刀", 3, feature[2]);
+  var weapon = new Weapon("紫光冰魄刀", 3, feature[4]);
   var armor = new Armor("荆棘之甲", 3);
 
   var soldier = new Soldier("张三", 100, 10, weapon, armor);
+
   var civilian = new Civilian("李四", 100, 5, "普通人");
 
   Pk.startPk(soldier, civilian);
