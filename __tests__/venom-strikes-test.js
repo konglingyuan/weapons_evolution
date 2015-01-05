ecccbc0f0f7e8f6ddd69e6a24ca040ad71a38cd2
lykong <konglingy_lady@sina.com>
2015-01-05 11:30:33 +0800

    Modified venom-strikes-test.js and test pass.

--- __tests__/venom-strikes-test.js
+++ __tests__/venom-strikes-test.js
@@ -3,6 +3,7 @@ jest.dontMock("../js/model/civilian.js");
 jest.dontMock("../js/model/weapon.js");
 jest.dontMock("../js/model/player");
 jest.dontMock("../js/model/weapon-feature");
+jest.dontMock("../js/model/armor");
 
 describe("Soldier", function() {
   describe(".venomStrikes", function() {
@@ -11,20 +12,23 @@ describe("Soldier", function() {
     var Weapon = require("../js/model/weapon");
     var Civilian = require("../js/model/civilian");
     var WeaponFeature = require("../js/model/weapon-feature");
+    var Armor = require("../js/model/armor");
 
     it("should return the correct statement", function() {
       var feature =new WeaponFeature("毒", 2, 1);
 
       var weapon = new Weapon("电光毒龙钻", 3, feature);
+      var armor = new Armor("荆棘之甲", 3);
 
-      var soldier = new Soldier("张三", 100, 20, weapon, "荆棘之甲");
+      var soldier = new Soldier("张三", 100, 20, weapon, armor);
 
       var civilian = new Civilian("李四", 100, 10);
 
       var result = soldier.venomStrikes(civilian);
 
       var str = "战士张三用电光毒龙钻攻击了普通人李四，李四受到了20点伤害，李四中毒了，生命值还剩：80" + "\n" +
-      "李四受到了3点毒液伤害，李四生命值还剩：77";
+          "李四受到了3点毒液伤害，李四生命值还剩：77" + "\n" +
+          "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93";
       expect(result).toBe(str);
     });
 
@@ -32,14 +36,16 @@ describe("Soldier", function() {
       var feature =new WeaponFeature("毒", 2, 0);
 
       var weapon = new Weapon("电光毒龙钻", 3, feature);
+      var armor = new Armor("荆棘之甲", 3);
 
-      var soldier = new Soldier("张三", 100, 20, weapon, "荆棘之甲");
+      var soldier = new Soldier("张三", 100, 20, weapon, armor);
 
       var civilian = new Civilian("李四", 100, 10);
 
       var result = soldier.venomStrikes(civilian);
 
-      var str = "战士张三用电光毒龙钻攻击了普通人李四，李四受到了20点伤害，李四生命值还剩：80";
+      var str = "战士张三用电光毒龙钻攻击了普通人李四，李四受到了20点伤害，李四生命值还剩：80" + "\n" +
+          "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93";
       expect(result).toBe(str);
     });
   });
