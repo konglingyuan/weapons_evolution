4918ee025c0aecf1af634d00c137641cbc209003
lykong <konglingy_lady@sina.com>
2015-01-05 09:55:18 +0800

    Delete spece and format.

--- js/main.js
+++ js/main.js
@@ -5,19 +5,16 @@ var Soldier = require("./model/soldier.js");
 var Civilian = require("./model/civilian.js");
 var WeaponFeature = require("./model/weapon-feature.js");
 
-
 game();
-function game() {
 
+function game() {
   var feature = WeaponFeature.all();
 
   var weapon = new Weapon("紫光冰魄刀", 3, feature[2]);
   var armor = new Armor("荆棘之甲", 3);
 
   var soldier = new Soldier("张三", 100, 10, weapon, armor);
-
   var civilian = new Civilian("李四", 100, 5, "普通人");
-
 
   Pk.startPk(soldier, civilian);
 }
