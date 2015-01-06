ff8d075696191a97b540114b98d3207319ddd299
lykong <konglingy_lady@sina.com>
2015-01-06 11:15:18 +0800

    Invoke function civilian.civilianBeat().

--- js/model/pk.js
+++ js/model/pk.js
@@ -5,44 +5,45 @@ function Pk() {
 }
 
 Pk.startPk = function (soldier,civilian) {
-    var resultText = "";
-    var features = soldier.weapon.feature;
+  var resultText = "";
+  var features = soldier.weapon.feature;
 
-    while(true){
-        if(soldier.hp <= 0) {
-            resultText += soldier.name + "被打败了！！！";
-            break;
-        }
-        this.getAttackMethod(features, soldier, civilian);
-
-        if(civilian.hp <= 0) {
-            resultText += civilian.name + "被打败了！！！";
-            break;
-        }
+  while(true){
+    if(soldier.hp <= 0) {
+      resultText += soldier.name + "被打败了！！！";
+      break;
     }
-    console.log(resultText);
+    this.getAttackMethod(features, soldier, civilian);
+
+    if(civilian.hp <= 0) {
+      resultText += civilian.name + "被打败了！！！";
+      break;
+    }
+    civilian.civilianBeat(soldier, soldier.armor);
+  }
+  console.log(resultText);
 };
 
 Pk.getAttackMethod = function (feature, soldier, civilian) {
-    if(feature.name === '致命一击') {
-        return soldier.criticalStrikes(civilian);
-    }
+  if(feature.name === '致命一击') {
+    return soldier.criticalStrikes(civilian);
+  }
 
-    if(feature.name === '火') {
-        return soldier.fireStrikes(civilian);
-    }
+  if(feature.name === '火') {
+    return soldier.fireStrikes(civilian);
+  }
 
-    if(feature.name === '毒') {
-        return soldier.venomStrikes(civilian);
-    }
+  if(feature.name === '毒') {
+    return soldier.venomStrikes(civilian);
+  }
 
-    if(feature.name === '晕') {
-        return soldier.dizzyStrikes(civilian);
-    }
+  if(feature.name === '晕') {
+    return soldier.dizzyStrikes(civilian);
+  }
 
-    if(feature.name === '冰') {
-        return soldier.iceStrikes(civilian);
-    }
+  if(feature.name === '冰') {
+    return soldier.iceStrikes(civilian);
+  }
 };
 
 module.exports = Pk;
