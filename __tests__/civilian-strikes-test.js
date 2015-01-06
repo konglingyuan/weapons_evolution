dc53cf60e422a3a1ed35f8867c4ac34c7f2989af
lykong <konglingy_lady@sina.com>
2015-01-06 23:25:04 +0800

    Delete useless code and format.

--- __tests__/civilian-strikes-test.js
+++ __tests__/civilian-strikes-test.js
@@ -5,19 +5,16 @@ jest.dontMock("../js/model/player");
 describe("Civilian", function() {
   describe(".civilianBeat", function() {
     it("should return the correct statement", function() {
-      var Player = require("../js/model/player");
       var Soldier = require("../js/model/soldier");
       var Civilian = require("../js/model/civilian");
 
       var armor = {name : '荆棘之甲', defense : 3};
-
-      var soldier = new Soldier("张三", 100, 20, "", armor);
-
-      var civilian = new Civilian("李四", 100, 10);
+      var soldier = new Soldier("张三", 100, 10, "战士", 0, "", armor);
+      var civilian = new Civilian("李四", 100, 5, "普通人", 0);
 
       var result = civilian.civilianBeat(soldier,armor);
+      var str = "普通人李四攻击了战士张三,战士张三受到了2点伤害,战士张三生命值还剩98";
 
-      var str = "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93"
       expect(result).toBe(str);
     });
   });
