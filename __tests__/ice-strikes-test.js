4aa7dfe9029241fa915ae4b70e4d359fc30318ad
lykong <konglingy_lady@sina.com>
2015-01-06 23:33:15 +0800

    Add property,format.

--- __tests__/ice-strikes-test.js
+++ __tests__/ice-strikes-test.js
@@ -12,39 +12,33 @@ describe("Soldier", function() {
     var Weapon = require("../js/model/weapon");
     var Civilian = require("../js/model/civilian");
     var WeaponFeature = require("../js/model/weapon-feature");
-      var Armor = require("../js/model/armor");
+    var Armor = require("../js/model/armor");
 
     it("should return the correct statement", function() {
         var feature = new WeaponFeature("冰", 1, 1);
-
         var weapon = new Weapon("紫光冰魄刀", 3, feature);
         var armor = new Armor("荆棘之甲", 3);
-
-        var soldier = new Soldier("张三", 100, 20, weapon, armor);
-
-        var civilian = new Civilian("李四", 100, 10);
+        var soldier = new Soldier("张三", 100, 10, "战士", 0, weapon, armor);
+        var civilian = new Civilian("李四", 100, 5, "普通人", 0);
 
         var result = soldier.iceStrikes(civilian);
+        var str = "战士张三用紫光冰魄刀攻击了普通人李四，李四受到了10点伤害，李四冻僵了，生命值还剩：90" + "\n" +
+            "普通人李四攻击了战士张三,战士张三受到了2点伤害,战士张三生命值还剩98";
 
-        var str = "战士张三用紫光冰魄刀攻击了普通人李四，李四受到了20点伤害，李四冻僵了，生命值还剩：80" + "\n" +
-            "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93";
         expect(result).toBe(str);
     });
 
     it("should return the correct statement", function() {
         var feature = new WeaponFeature("冰", 1, 0);
-
         var weapon = new Weapon("紫光冰魄刀", 3, feature);
         var armor = new Armor("荆棘之甲", 3);
-
-        var soldier = new Soldier("张三", 100, 20, weapon, armor);
-
-        var civilian = new Civilian("李四", 100, 10);
+        var soldier = new Soldier("张三", 100, 10, "战士", 0, weapon, armor);
+        var civilian = new Civilian("李四", 100, 5, "普通人", 0);
 
         var result = soldier.iceStrikes(civilian);
+        var str = "战士张三用紫光冰魄刀攻击了普通人李四，李四受到了10点伤害，李四生命值还剩：90" + "\n" +
+            "普通人李四攻击了战士张三,战士张三受到了2点伤害,战士张三生命值还剩98";
 
-        var str = "战士张三用紫光冰魄刀攻击了普通人李四，李四受到了20点伤害，李四生命值还剩：80" + "\n" +
-            "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93";
         expect(result).toBe(str);
     });
   });
