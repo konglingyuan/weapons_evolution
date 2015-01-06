14a7c0888de7f0bb2a96082a893bbb188baa9b47
lykong <konglingy_lady@sina.com>
2015-01-06 11:07:19 +0800

    Add property state.

--- js/model/player.js
+++ js/model/player.js
@@ -1,8 +1,9 @@
-function Player(name, hp, attack, profession) {
+function Player(name, hp, attack, profession, state) {
   this.name = name;
   this.hp = hp;
   this.attack = attack;
   this.profession = profession;
+  this.state = state;
 }
 
 module.exports = Player;
