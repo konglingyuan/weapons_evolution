ca90a3099cfbad74e09804aab309278e26021c8d
lykong <konglingy_lady@sina.com>
2015-01-06 23:36:37 +0800

    Change property value.

--- js/main.js
+++ js/main.js
@@ -10,12 +10,12 @@ game();
 function game() {
   var feature = WeaponFeature.all();
 
-  var weapon = new Weapon("火箭", 3, feature[1]);
+  var weapon = new Weapon("火箭", 3, feature[3]);
   var armor = new Armor("荆棘之甲", 3);
 
-  var soldier = new Soldier("张三", 100, 10, weapon, armor);
+  var soldier = new Soldier("张三", 100, 10, "战士", 0, weapon, armor);
 
-  var civilian = new Civilian("李四", 100, 5, "普通人");
+  var civilian = new Civilian("李四", 100, 5, "普通人", 0);
 
   Pk.startPk(soldier, civilian);
 }
