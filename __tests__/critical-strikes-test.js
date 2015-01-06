d12784b54f491b6bcf45061cba1de22273bb2334
lykong <konglingy_lady@sina.com>
2015-01-06 23:26:26 +0800

    Delete useless code,add player's property,format.

--- __tests__/critical-strikes-test.js
+++ __tests__/critical-strikes-test.js
@@ -15,36 +15,27 @@ describe("Soldier", function() {
 
     it("should return the correct statement", function() {
       var feature = new WeaponFeature("致命一击", 1, 1);
-
       var weapon = new Weapon("暴剑", 3, feature);
-
       var armor = new Armor("荆棘之甲", 3);
-      var soldier = new Soldier("张三", 100, 20, weapon, armor);
-
-      var civilian = new Civilian("李四", 100, 10);
+      var soldier = new Soldier("张三", 100, 10, "战士", 0, weapon, armor);
+      var civilian = new Civilian("李四", 100, 5, "普通人", 0);
 
       var result = soldier.criticalStrikes(civilian);
+      var str = "战士张三用暴剑攻击了普通人李四,战士张三发动了致命一击,李四受到了39点伤害,李四生命值还剩：61\n";
 
-      var str = "战士张三用暴剑攻击了普通人李四,战士张三发动了致命一击,李四受到了69点伤害,李四生命值还剩：31" + "\n" +
-          "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93";
       expect(result).toBe(str);
     });
-    
+
     it("should return the correct statement", function() {
       var feature = new WeaponFeature("致命一击", 1, 0);
-
       var weapon = new Weapon("暴剑", 3, feature);
-
       var armor = new Armor("荆棘之甲", 3);
-
-      var soldier = new Soldier("张三", 100, 20, weapon, armor);
-
-      var civilian = new Civilian("李四", 100, 10);
+      var soldier = new Soldier("张三", 100, 10, "战士", 0, weapon, armor);
+      var civilian = new Civilian("李四", 100, 5, "普通人", 0);
 
       var result = soldier.criticalStrikes(civilian);
+      var str = "战士张三用暴剑攻击了普通人李四,李四受到了10点伤害,李四生命值还剩：90\n";
 
-      var str = "战士张三用暴剑攻击了普通人李四,李四受到了20点伤害,李四生命值还剩：80" + "\n" +
-          "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93";
       expect(result).toBe(str);
     });
   });
