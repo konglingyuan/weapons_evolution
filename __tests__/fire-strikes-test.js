edc7ba2181f8e6d64cf283adf057cfb4f9d48288
lykong <konglingy_lady@sina.com>
2015-01-06 23:30:20 +0800

    Modified fire-strikes-test.js test,add player property,delete useless code, format.

--- __tests__/fire-strikes-test.js
+++ __tests__/fire-strikes-test.js
@@ -4,6 +4,7 @@ jest.dontMock("../js/model/weapon.js");
 jest.dontMock("../js/model/player");
 jest.dontMock("../js/model/weapon-feature");
 jest.dontMock("../js/model/armor");
+jest.dontMock("../js/model/state");
 
 describe("Soldier", function() {
   describe(".fireStrikes", function() {
@@ -13,39 +14,35 @@ describe("Soldier", function() {
     var Civilian = require("../js/model/civilian");
     var WeaponFeature = require("../js/model/weapon-feature");
     var Armor = require("../js/model/armor");
+    var State = require("../js/model/state");
 
     it("should return the correct statement", function() {
       var feature = new WeaponFeature("火", 2, 1);
-
       var weapon = new Weapon("火箭", 3, feature);
       var armor = new Armor("荆棘之甲", 3);
+      var soldier = new Soldier("张三", 100, 10, "战士", 0, weapon, armor);
+      var civilian = new Civilian("李四", 100, 5, "普通人", 0);
 
-      var soldier = new Soldier("张三", 100, 20, weapon, armor);
-
-      var civilian = new Civilian("李四", 100, 10);
+      var State = jest.genMockFn();
+      State.mockReturnValue(civilian, weapon, 1);
 
       var result = soldier.fireStrikes(civilian);
+      var str = "战士张三用火箭攻击了普通人李四，李四受到了10点伤害，李四着火了，生命值还剩：90" + "\n" +
+         "普通人李四受到了3点火伤害，李四生命值还剩：87";
 
-      var str = "战士张三用火箭攻击了普通人李四，李四受到了20点伤害，李四着火了，生命值还剩：80" + "\n" +
-         "李四受到了3点火焰伤害，李四生命值还剩：77" + "\n" +
-          "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93";
       expect(result).toBe(str);
     });
 
     it("should return the correct statement", function() {
       var feature = new WeaponFeature("火", 2, 0);
-
       var weapon = new Weapon("火箭", 3, feature);
       var armor = new Armor("荆棘之甲", 3);
-
-      var soldier = new Soldier("张三", 100, 20, weapon, armor);
-
-      var civilian = new Civilian("李四", 100, 10);
+      var soldier = new Soldier("张三", 100, 10, "战士", 0, weapon, armor);
+      var civilian = new Civilian("李四", 100, 5, "普通人", 0);
 
       var result = soldier.fireStrikes(civilian);
+      var str = "战士张三用火箭攻击了普通人李四，李四受到了10点伤害，李四生命值还剩：90" + "\n";
 
-      var str = "战士张三用火箭攻击了普通人李四，李四受到了20点伤害，李四生命值还剩：80" + "\n" +
-          "普通人李四攻击了战士张三,战士张三受到了7点伤害,战士张三生命值还剩93";
       expect(result).toBe(str);
     });
   });
